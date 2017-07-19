65301d0da3f3369de469ff8a16f8bd96612caf97
KhorinVitaly <khorin.v@gmail.com>
2017-07-19 14:27:07 +0600

    Опять Валли
    Пришлось везде убрать переменную data, а то Валли лютует

--- bars.py
+++ bars.py
@@ -6,16 +6,16 @@ from sys import argv
 
 def load_data(filepath):
     with open(filepath, "r", encoding='utf-8') as json_file:
-        data = json_file.read()
-        return json.loads(data)
+        json_data = json_file.read()
+        return json.loads(json_data)
 
 
-def get_biggest_bar(data):
-    return max(data, key=lambda x: x["SeatsCount"])
+def get_biggest_bar(json_data):
+    return max(json_data, key=lambda x: x["SeatsCount"])
 
 
-def get_smallest_bar(data):
-    return min(data, key=lambda x: x["SeatsCount"])
+def get_smallest_bar(json_data):
+    return min(json_data, key=lambda x: x["SeatsCount"])
 
 
 def get_distance(latitude1, longitude1, latitude2, longitude2):
@@ -29,9 +29,9 @@ def check_of_coordinates(latitude, longitude):
         raise ValueError()
 
 
-def get_closest_bar(data, latitude, longitude):
+def get_closest_bar(json_data, latitude, longitude):
     # Есть сомнения на сколько строка ниже читаема, но зато нет цикла, есть лишь один вызов функции min
-    return min(data, key=lambda x: get_distance(latitude, longitude, float(x["Latitude_WGS84"]),
+    return min(json_data, key=lambda x: get_distance(latitude, longitude, float(x["Latitude_WGS84"]),
                                                 float(x["Longitude_WGS84"])))
 
 
@@ -43,8 +43,8 @@ def print_bar(bar_item):
 def fetch_data():
     try:
         filepath = argv[1]
-        data = load_data(filepath)
-        return data
+        json_data = load_data(filepath)
+        return json_data
     except FileNotFoundError:
         print("File not found!")
     except IndexError:
@@ -54,24 +54,24 @@ def fetch_data():
     exit()
 
 
-def print_smallest_biggest_and_closest_bars(data):
+def print_smallest_biggest_and_closest_bars(json_data):
     print("Smallest bar: ", end='')
-    print_bar(get_smallest_bar(data))
+    print_bar(get_smallest_bar(json_data))
     print("Biggest bar: ", end='')
-    print_bar(get_biggest_bar(data))
+    print_bar(get_biggest_bar(json_data))
 
     print("Input your current coordinates")
     latitude = float(input("latitude: "))
     longitude = float(input("longitude: "))
     check_of_coordinates(latitude, longitude)
     print("Closest bar: ", end='')
-    print_bar(get_closest_bar(data, latitude, longitude))
+    print_bar(get_closest_bar(json_data, latitude, longitude))
 
 
 if __name__ == '__main__':
-    text = fetch_data()
+    json_data = fetch_data()
     try:
-        print_smallest_biggest_and_closest_bars(text)
+        print_smallest_biggest_and_closest_bars(json_data)
     except TypeError:
         print("File format error!")
     except ValueError:
