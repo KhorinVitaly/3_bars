43cc0f0f05ecd57d4ecb6954f8a68716866e7618
KhorinVitaly <khorin.v@gmail.com>
2017-07-20 22:38:29 +0600

    use partial, format and separate some function
    for fix my mistakes and improve code readability

--- bars.py
+++ bars.py
@@ -2,10 +2,11 @@ import json
 from geopy import Point
 from geopy.distance import distance
 from sys import argv
+from functools import partial
 
 
 def load_data(filepath):
-    with open(filepath, "r", encoding='utf-8') as json_file:
+    with open(filepath, "r", encoding='cp1251') as json_file:
         json_data = json_file.read()
         return json.loads(json_data)
 
@@ -30,14 +31,18 @@ def check_of_coordinates(latitude, longitude):
 
 
 def get_closest_bar(json_data, latitude, longitude):
-    # Есть сомнения на сколько строка ниже читаема, но зато нет цикла, есть лишь один вызов функции min
-    return min(json_data, key=lambda x: get_distance(latitude, longitude, float(x["Latitude_WGS84"]),
-                                                float(x["Longitude_WGS84"])))
+    call_func = partial(get_distance, latitude, longitude)
+    return min(json_data, key=lambda x: call_func(float(x["Latitude_WGS84"]), float(x["Longitude_WGS84"])))
 
 
 def print_bar(bar_item):
-    print(bar_item["Name"] + "; Address: " + bar_item["AdmArea"] + ", " + bar_item["District"] + ", "
-          + bar_item["Address"] + "; Public phone: " + bar_item["PublicPhone"][0]["PublicPhone"])
+   name = bar_item["Name"]
+   adm_area = bar_item["AdmArea"]
+   district = bar_item["District"]
+   address = bar_item["Address"]
+   phone = bar_item["PublicPhone"][0]["PublicPhone"]
+
+   print('Name: {0}; Address: {1}, {2}, {3}; Phone {4}'.format(name, adm_area, district, address, phone))
 
 
 def fetch_data():
@@ -54,16 +59,19 @@ def fetch_data():
     exit()
 
 
-def print_smallest_biggest_and_closest_bars(json_data):
-    print("Smallest bar: ", end='')
-    print_bar(get_smallest_bar(json_data))
-    print("Biggest bar: ", end='')
-    print_bar(get_biggest_bar(json_data))
-
+def intput_coordinates():
     print("Input your current coordinates")
     latitude = float(input("latitude: "))
     longitude = float(input("longitude: "))
     check_of_coordinates(latitude, longitude)
+    return latitude, longitude
+
+
+def print_smallest_biggest_and_closest_bars(json_data, latitude, longitude):
+    print("Smallest bar: ", end='')
+    print_bar(get_smallest_bar(json_data))
+    print("Biggest bar: ", end='')
+    print_bar(get_biggest_bar(json_data))
     print("Closest bar: ", end='')
     print_bar(get_closest_bar(json_data, latitude, longitude))
 
@@ -71,7 +79,8 @@ def print_smallest_biggest_and_closest_bars(json_data):
 if __name__ == '__main__':
     json_data = fetch_data()
     try:
-        print_smallest_biggest_and_closest_bars(json_data)
+        latitude, longitude = intput_coordinates()
+        print_smallest_biggest_and_closest_bars(json_data, latitude, longitude)
     except TypeError:
         print("File format error!")
     except ValueError:
